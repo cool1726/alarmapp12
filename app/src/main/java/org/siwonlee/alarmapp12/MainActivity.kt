f46152cc15cc99399c2b6b1dfddb88aa6bb319b9
themion@kau.kr <themion@kau.kr>
2019-09-18 00:15:53 +0900

    TimePicker를 이용해 24시간 기준으로 특정 시간을 지정한 뒤 버튼을 누르면 지정한 시간이 출력되도록 설정함

--- app/src/main/java/org/siwonlee/alarmapp12/MainActivity.kt
+++ app/src/main/java/org/siwonlee/alarmapp12/MainActivity.kt
@@ -2,11 +2,44 @@ package org.siwonlee.alarmapp12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Build
+import android.widget.TimePicker
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    var cal : Calendar = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var hr : Int = cal.get(Calendar.HOUR_OF_DAY)
+        var min : Int = cal.get(Calendar.MINUTE)
+
+        val timePicker: TimePicker = findViewById(R.id.timePicker)
+        timePicker.setIs24HourView(true)
+        timePicker.setOnTimeChangedListener({timePicker, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+            cal.set(Calendar.SECOND, 0)
+
+            hr = hour; min = minute;
+        })
+
+        val set: Button = findViewById(R.id.button)
+        set.setOnClickListener{
+            val str : String = hr.toString() + ":" + min.toString()
+            Toast.makeText(this@MainActivity, str, Toast.LENGTH_SHORT).show()
+        }
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            timePicker.setHour(hr)
+            timePicker.setMinute(min)
+        } else {
+            timePicker.setCurrentHour(hr)
+            timePicker.setCurrentMinute(min)
+        }
     }
 }
