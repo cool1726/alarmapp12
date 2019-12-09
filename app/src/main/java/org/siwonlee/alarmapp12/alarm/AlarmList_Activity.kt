b6ba67148a8f6bdaa7b82c59e7178f4fd1c55edc
cool1726 <cool1726@naver.com>
2019-12-09 17:54:13 +0900

    bottom navigation bar (2) - 프래그먼트 작업

--- app/src/main/java/org/siwonlee/alarmapp12/alarm/AlarmList_Activity.kt
+++ app/src/main/java/org/siwonlee/alarmapp12/alarm/AlarmList_Activity.kt
@@ -1,14 +1,24 @@
 package org.siwonlee.alarmapp12.alarm
 
+import android.app.Activity
+import android.content.Context.MODE_PRIVATE
+import android.content.Intent
 import android.os.Bundle
 import java.util.*
 import android.view.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import org.siwonlee.alarmapp12.R
-import org.siwonlee.alarmapp12.UserData
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.alarm_list.*
+import kotlinx.android.synthetic.main.fragment_main.*
+import org.siwonlee.alarmapp12.*
+import org.siwonlee.alarmapp12.friends.GoogleSignInActivity
+import org.siwonlee.alarmapp12.information.GetWeatherInfo
 
 class AlarmList_Activity : Fragment() {
     val ALARM_SET: Int = 1000
@@ -23,22 +33,21 @@ class AlarmList_Activity : Fragment() {
 
     var categorize: ArrayList<String> = ArrayList()
 
-    private lateinit var alarmViewModel: AlarmViewModel
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        alarmViewModel = ViewModelProviders.of(this).get(AlarmViewModel::class.java)
-        val root = inflater.inflate(R.layout.alarm_list, container, false)
-        return root
+    companion object {
+        fun newInstance() = AlarmList_Activity()
     }
 
-   // override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-       // inflater.inflate(R.layout.google_sign_in_activity, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.alarm_list, container, false)
+        return view
+    }
 
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.alarm_list)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val pref = this.getSharedPreferences(prefStorage, MODE_PRIVATE)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.alarm_recyclerview)
+
+        val pref = activity?.getSharedPreferences(prefStorage, MODE_PRIVATE)
         val editor = pref!!.edit()
         //editor.clear() pref에 등록된 알람 데이터 삭제용
 
@@ -63,7 +72,7 @@ class AlarmList_Activity : Fragment() {
         sp_category.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
             override fun onItemSelected(adapterView: AdapterView<*>, view: View, i: Int, l: Long ) {
                 currentCategory = categorize[i]
-                alarm_recyclerview.adapter = makeAdapter()
+                recyclerView.adapter = makeAdapter()
             }
             override fun onNothingSelected(adapterView: AdapterView<*>)  {
                 sp_category.setSelection(0)
@@ -71,13 +80,14 @@ class AlarmList_Activity : Fragment() {
         }
 
         // LinearLayoutManager : alarm_list.xml의 alarm_recyclerview에 세로형태로 아이템을 배치한다
-        alarm_recyclerview.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        alarm_recyclerview.setHasFixedSize(true)
-        alarm_recyclerview.adapter = makeAdapter()
+
+        recyclerView.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = makeAdapter()
 
         // 알람 초기 셋팅 : 알람 셋팅 화면으로 이동 (activity_main.xml로 이동)
         fab_add.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(activity, Alarm_Set::class.java)
             intent.putExtra("categories", alarmlist.category())
             //알람을 설정한다
             startActivityForResult(intent, this.ALARM_SET)
@@ -87,25 +97,25 @@ class AlarmList_Activity : Fragment() {
     // item을 오래 클릭할 시 바로 알람 삭제여부를 묻는 dialog 띄우기 (나중에)
     fun onLongClickEvent() { }
 
-    // 특정 REQUEST_CODE로 MainActivity.kt에 결과값을 요구하고 결과값 받기
+    // 특정 REQUEST_CODE로 Alarm_set.kt에 결과값을 요구하고 결과값 받기
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        val pref = this.getSharedPreferences(prefStorage, MODE_PRIVATE)
+        val pref = activity?.getSharedPreferences(prefStorage, MODE_PRIVATE)
         val editor = pref!!.edit()
 
-        if (resultCode == Activity.RESULT_OK) { // MainActivity에서 RESULT_OK 사인을 보내면
+        if (resultCode == Activity.RESULT_OK) { // Alarm_Set에서 RESULT_OK 사인을 보내면
             when (requestCode) {
                 //알람을 설정, 수정, 혹은 삭제했을 경우
                 ALARM_SET -> {
-                    // MainActivity에서 보낼 수 있는 모든 정보를 모은다
+                    // Alarm_set에서 보낼 수 있는 모든 정보를 모은다
                     val strData = data!!.getStringExtra("data")
                     val before = data.getStringExtra("before")
 
                     //알람을 수정하거나 삭제했다면 before_id는 -1이 아니다
                     if(before != null) {
                         val popData = GsonBuilder().create().fromJson(before, Alarm_Data::class.java)
-                        alarmlist.unset(this)
+                        alarmlist.unset(activity!!.applicationContext)
                         //alarmlist에서 알람을 제거한다
                         alarmlist.pop(popData)
                     }
@@ -116,7 +126,7 @@ class AlarmList_Activity : Fragment() {
                         val data = GsonBuilder().create().fromJson(strData, Alarm_Data::class.java)
                         //alarmlist에 data를 추가한 뒤 알람을 갱신한다
                         alarmlist.add(data)
-                        alarmlist.set(this)
+                        alarmlist.set(activity!!.applicationContext)
                     }
 
                     //리스트에 변경이 있었으므로 알람을 정렬한다
@@ -131,11 +141,11 @@ class AlarmList_Activity : Fragment() {
                     //해당 데이터가 "", 즉 이상한 데이터가 아니라면
                     if(fetchUserData != "") {
                         //기존에 존재하는 모든 알람을 해제한 뒤
-                        alarmlist.unset(this)
+                        alarmlist.unset(activity!!.applicationContext)
                         //백업한 데이터를 가져와 alarmlist에 저장하고
                         alarmlist = GsonBuilder().create().fromJson(fetchUserData, UserData::class.java)
                         //백업한 리스트의 모든 알람을 set한 다음 정렬한다
-                        alarmlist.set(this)
+                        alarmlist.set(activity!!.applicationContext)
                         alarmlist.sort()
                     }
                 }
@@ -160,11 +170,11 @@ class AlarmList_Activity : Fragment() {
         editor.apply()
     }
 
-    //로그인 등의 메뉴를 앱의 메뉴바에 표시
+    /*//로그인 등의 메뉴를 앱의 메뉴바에 표시
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.alarm_list_menu, menu)
         return super.onCreateOptionsMenu(menu)
-    }
+    }*/
 
     //메뉴의 각 옵션을 선택했을 때 실행할 동작
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -172,7 +182,7 @@ class AlarmList_Activity : Fragment() {
             //계정 연동 관리 버튼을 눌렀을 때
             R.id.signin -> {
                 //GoogleSignInActivity를 실행시킬 intent를 만든 뒤
-                val logInIntent = Intent(this, GoogleSignInActivity::class.java)
+                val logInIntent = Intent(activity, GoogleSignInActivity::class.java)
 
                 //alarmlist를 gson을 이용해 String타입으로 형변환해 logInIntent에 넣는다
                 val strList = GsonBuilder().create().toJson(alarmlist, UserData::class.java)
@@ -184,12 +194,12 @@ class AlarmList_Activity : Fragment() {
             //날씨 정보 버튼을 눌렀을 때
             R.id.weather -> {
                 //GetWeatherInfo를 실행시킬 intent를 만들고
-                val weatherIntent = Intent(this, GetWeatherInfo::class.java)
+                val weatherIntent = Intent(activity, GetWeatherInfo::class.java)
                 //현재 위치정보를 intent로 전달하는 부분 필요함
                 startActivityForResult(weatherIntent, WEATHER_INFO)
             }
             R.id.mapAlarm -> {
-                val mapIntent = Intent(this, Alarm_Map::class.java)
+                val mapIntent = Intent(activity, Alarm_Map::class.java)
                 val strSet = GsonBuilder().create().toJson(alarmlist.markerSet, Marker_Set::class.java)
                 mapIntent.putExtra("set", strSet)
                 //알람을 설정한다
@@ -204,7 +214,7 @@ class AlarmList_Activity : Fragment() {
         categorize.add("전체 카테고리")
 
         val ret = ArrayAdapter(
-            this,
+            activity!!.applicationContext,
             android.R.layout.simple_spinner_dropdown_item,
             categorize
         )
@@ -216,8 +226,8 @@ class AlarmList_Activity : Fragment() {
         // AlarmlistAdapter의 ViewHolder
         val smallList = alarmlist.getCategoryList(currentCategory)
         // 선택된 category에 해당하는 alarmlist를 전달하기 위해 getCategoryList 함수 이용
-        val adapter = AlarmListAdapter(this, smallList, { position ->
-            val cintent = Intent(this, MainActivity::class.java)
+        val adapter = AlarmListAdapter(activity!!.applicationContext, smallList, { position ->
+            val cintent = Intent(activity, Alarm_Set::class.java)
 
             var newPos = 0
 
@@ -237,11 +247,8 @@ class AlarmList_Activity : Fragment() {
             //알람을 수정한다
             startActivityForResult(cintent, this.ALARM_SET)
         },
-            { position -> onLongClickEvent()
-            })
+            { position -> onLongClickEvent() })
 
         return adapter
     }
-
-*/
 }
--- app/src/main/java/org/siwonlee/alarmapp12/information/GetWeatherInfo.kt
+++ app/src/main/java/org/siwonlee/alarmapp12/information/GetWeatherInfo.kt
@@ -1,14 +1,29 @@
 package org.siwonlee.alarmapp12.information
 
+import android.content.Context
 import android.os.Bundle
 import java.text.SimpleDateFormat
 import java.util.*
 import android.location.LocationManager
+import android.os.AsyncTask
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
 import org.siwonlee.alarmapp12.R
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import org.xml.sax.InputSource
+import java.net.URL
+import javax.xml.parsers.DocumentBuilderFactory
 
 
 class GetWeatherInfo : Fragment() {
@@ -32,13 +47,246 @@ class GetWeatherInfo : Fragment() {
     var dlatitude : Double = 0.0
     var dlongitude : Double = 0.0
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.weather_after_ringing, container, false)
-/*
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.weather_after_ringing, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(ntime < 210) {
+            weatherDate = (weatherDate.toInt() - 1).toString()
+            weatherTime = "2300"  }
+        else if (ntime < 510) weatherTime = "0200"
+        else if (ntime < 810) weatherTime = "0500"
+        else if (ntime < 1110) weatherTime = "0800"
+        else if (ntime < 1410) weatherTime = "1100"
+        else if (ntime < 1710) weatherTime = "1400"
+        else if (ntime < 2010) weatherTime = "1700"
+        else if (ntime < 2310) weatherTime = "2000"
+        else if (ntime < 2400) weatherTime = "2300"
+
+        lateinit var mFusedLocationClient: FusedLocationProviderClient
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this.activity!!)
+
+        //locationManager 이용한 현재 내 위치 찾기
+        lm = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+
+        mFusedLocationClient.lastLocation.addOnSuccessListener {
+            if(it != null) {
+                val myLocation = LatLng(it.latitude, it.longitude)
+                dlatitude = myLocation.latitude
+                dlongitude = myLocation.longitude
+                Log.d("myLocation", "myLocation : ${dlatitude}, ${dlongitude}")
+
+                val tmp = convertGRID_GPS(dlatitude, dlongitude)
+                Log.d("위경도 -> 격자", "x = " + tmp.x + ", y = " + tmp.y)
+
+                val strLocate = view.findViewById<TextView>(R.id.location)
+                strLocate.setText("${tmp.x.toInt()}, ${tmp.y.toInt()}")
+
+
+                weather_url = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData?serviceKey=" +
+                        "4N926ah8IlK9Xzvu1O%2FlEZEbWJHqRQI0VgrqT6vR54oG0ADd%2B8MyeYhQGIvGvGLXc%2F%2F%2BzWpoWAuyKP8PLh87xw%3D%3D" +
+                        "&base_date=" + weatherDate + "&base_time=" + weatherTime +
+                        "00&nx=" + tmp.x.toInt() + "&ny=" + tmp.y.toInt() + "&numOfRows=10&pageNo=1&_type=xml"
+
+                GetXMLTask().execute()
+                Toast.makeText(context, "${weatherTime}", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        /*val mGeocoder = Geocoder(applicationContext, Locale.KOREAN)
+        var mResultList: List<Address>? = null
+        try {
+            mResultList = mGeocoder.getFromLocation(
+                dlatitude, dlongitude, 1
+            )
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        if (mResultList != null) {
+            //Log.d("CheckCurrentLocation", mResultList[0].getAddressLine(0))
+            //currentLocation = mResultList[0].getAddressLine(0)
+            //currentLocation = currentLocation.substring(5)  //'대한민국' 글자 빼기 위함
+
+        }*/
+    }
+
+    private inner class GetXMLTask() : AsyncTask<String, Void, Document>() {
+        var txt_weather: TextView = view!!.findViewById(R.id.txt_weather)
+        var weatherImage = view!!.findViewById<ImageView>(R.id.weatherImage)
+        var rainAmount = view!!.findViewById<TextView>(R.id.rainamount)
+        var temperature = view!!.findViewById<TextView>(R.id.temperature)
+        var updateT = view!!.findViewById<TextView>(R.id.updateTime)
+
+        override fun doInBackground(vararg urls: String): Document? {
+            val url: URL
+            var doc: Document? = null
+            try {
+                url = URL(weather_url)
+                val dbf = DocumentBuilderFactory.newInstance()
+                val db = dbf.newDocumentBuilder()
+                doc = db.parse(InputSource(url.openStream()))
+                doc!!.getDocumentElement().normalize()
+            } catch (e: Exception) {
+                Toast.makeText(context, "Parsing Error", Toast.LENGTH_SHORT).show()
+            }
+            return doc
+        }
+        override fun onPostExecute(doc: Document) {
+            var s = ""
+            val nodeList = doc.getElementsByTagName("item")
+
+            s += "위치 : x : ${dlatitude}, y : ${dlongitude}" + "\n"
+            s += "시간 : ${weatherDate} ${weatherTime}" + " \n"
+            updateT.setText("${weatherTime.substring(0, 2)}시 ${weatherTime.substring(2, 4)}분 발표")
+
+            for (i in 0 until nodeList.getLength()) {
+                val node = nodeList.item(i)
+                val fstElmnt = node as Element
+                val idx = fstElmnt.getElementsByTagName("category")
+
+                val item = fstElmnt.getElementsByTagName("fcstValue").item(0).childNodes.item(0).nodeValue
+                val feature = idx.item(0).childNodes.item(0).nodeValue
+
+
+                // 최저기온
+                if (feature.equals("TMN")) {
+                    s += "최저기온 = " + item + "℃ \n"
+                }
+
+                // 최고기온
+                if (feature.equals("TMX")) {
+                    s += "최고기온 = " + item + "℃ \n"
+                }
+
+                // 강수확률% POP
+                if (feature.equals("POP"))
+                    s += "강수확률 = " + item + "% \n"
+
+                // 강수형태 PTY : 없음(0), 비(1), 진눈깨비(2), 눈(3), 소나기(4)
+                if (feature.equals("PTY")) {
+                    val rain = Integer.parseInt(item)
+                    when(rain) {
+                        0 -> {s += "강수상태 = 비 안 옴" + " \n"}
+                        1 -> {
+                            s += "강수상태 = 비" + " \n"
+                            weatherImage.setImageResource(R.drawable.rainy) }
+                        2 -> {
+                            s += "강수상태 = 진눈깨비" + " \n"
+                            weatherImage.setImageResource(R.drawable.rainy) }
+                        3 -> {
+                            s += "강수상태 = 눈" + " \n"
+                            weatherImage.setImageResource(R.drawable.snowy) }
+                        4 -> {
+                            s += "강수상태 = 소나기" + " \n"
+                            weatherImage.setImageResource(R.drawable.rainy) }
+                    }
+                }
+
+                // 강수량
+                if (feature.equals("R06")) {
+                    if(Integer.parseInt(item) > 0) {
+                        s += "강수량 = " + item + "mm \n"
+                        rainAmount.setText("${item}mm의 비가 예상됩니다")
+                    }
+                }
+
+                // 적설량
+                if (feature.equals("S06")) {
+                    if (Integer.parseInt(item) > 0) {
+                        s += "적설량 = " + item + "mm \n"
+                        rainAmount.setText("${item}cm의 눈이 예상됩니다")
+                    }
+                }
+
+                // 습도% REH
+                if (feature.equals("REH")) s += "습도 = " + item + "% \n"
+
+                // 기온℃ T3H
+                if (feature.equals("T3H")) {
+                    s += "온도 = " + item + "℃ \n"
+                    temperature.setText(item)
+                    Log.d("temperature", "temperature : ${item}")
+                }
+
+                // 구름상태 SKY : 맑음(1),  구름많음(3),  흐림(4)
+                if (feature.equals("SKY")) {
+                    val cloud_num =
+                        Integer.parseInt(item)
+
+                    if (cloud_num == 1) {
+                        s += "하늘상태 = 맑음\n"
+                        weatherImage.setImageResource(R.drawable.sunny)
+                    }
+                    else if (cloud_num == 3) s += "하늘상태 = 구름 많음\n"
+                    else if (cloud_num == 4) s += "하늘상태 = 흐림\n"
+                }
+                txt_weather.setText(s)
+
+                super.onPostExecute(doc)
+            }
+
+        }
 
     }
+
+    private fun convertGRID_GPS(lat_X: Double, lng_Y: Double): LatXLngY {
+        val re = 6371.00877 // 지구 반경(km)
+        val grid = 5.0 // 격자 간격(km)
+        val slat1 = 30.0 // 투영 위도1(degree)
+        val slat2 = 60.0 // 투영 위도2(degree)
+        val olon = 126.0 // 기준점 경도(degree)
+        val olat = 38.0 // 기준점 위도(degree)
+        val xo = 43.0 // 기준점 X좌표(GRID)
+        val yo = 136.0 // 기준점 Y좌표(GRID)
+
+        // LCC DFS 좌표변환 (lat_X:위도,  lng_Y:경도)
+        val degrad = Math.PI / 180.0
+
+        val re_grid = re / grid
+        val slat1_degrad = slat1 * degrad
+        val slat2_degrad = slat2 * degrad
+        val olon_degrad = olon * degrad
+        val olat_degrad = olat * degrad
+
+        var sn = Math.tan(Math.PI * 0.25 + slat2_degrad * 0.5) / Math.tan(Math.PI * 0.25 + slat1_degrad * 0.5)
+        sn = Math.log(Math.cos(slat1_degrad) / Math.cos(slat2_degrad)) / Math.log(sn)
+        var sf = Math.tan(Math.PI * 0.25 + slat1_degrad * 0.5)
+        sf = Math.pow(sf, sn) * Math.cos(slat1_degrad) / sn
+        var ro = Math.tan(Math.PI * 0.25 + olat_degrad * 0.5)
+        ro = re_grid * sf / Math.pow(ro, sn)
+        val rs = LatXLngY()
+
+        Log.d("lat_X, lng_Y", "${lat_X} , ${lng_Y}")
+
+        rs.lat = lat_X
+        rs.lng = lng_Y
+        var ra = Math.tan(Math.PI * 0.25 + lat_X * degrad * 0.5)
+        ra = re_grid * sf / Math.pow(ra, sn)
+        var theta = lng_Y * degrad - olon_degrad
+        if (theta > Math.PI) theta -= 2.0 * Math.PI
+        if (theta < -Math.PI) theta += 2.0 * Math.PI
+        theta *= sn
+        rs.x = Math.floor(ra * Math.sin(theta) + xo + 0.5)
+        rs.y = Math.floor(ro - ra * Math.cos(theta) + yo + 0.5)
+
+        Log.d("rs.lat, rs.lng", "${rs.lat} , ${rs.lng}")
+        Log.d("rs.x, rs.y", "${rs.x} , ${rs.y}")
+        return rs
+    }
+
+
+    internal inner class LatXLngY {
+        var lat: Double = 0.toDouble()
+        var lng: Double = 0.toDouble()
+
+        var x: Double = 0.toDouble()
+        var y: Double = 0.toDouble()
+    }
+
+/*
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
